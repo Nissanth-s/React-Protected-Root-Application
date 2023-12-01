e1ae654387acd37114d38d13e045b0b2ffed3397
Nissanth <nishpre@gmail.com>
2023-12-01 20:35:55 +0530

    Updates

---
+++ static/js/751.7981b26f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[751],{751:(s,a,e)=>{e.r(a),e.d(a,{default:()=>r});e(791);var p=e(184);const r=()=>(0,p.jsx)("div",{className:"pageNotFound",children:(0,p.jsx)("img",{src:"/404-error.jpg",alt:"404"})})}}]);
+//# sourceMappingURL=751.7981b26f.chunk.js.map
